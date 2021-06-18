a3634aa0b49b220a566bcd595d44dce735ace60d
kendrajmoore <kendra.j.moore3443@gmail.com>
2021-06-18 10:04:27 -0400

    working on form

--- app/__init__.py
+++ app/__init__.py
@@ -12,33 +12,33 @@ if not os.environ.get("PRODUCTION"):
 
 app = Flask(__name__)
 
-base_url = os.getenv("URL")
-projects_base_url = base_url + "/projects/"
-profiles_base_url = base_url + "/profiles/"
+# base_url = os.getenv("URL")
+# projects_base_url = base_url + "/projects/"
+# profiles_base_url = base_url + "/profiles/"
 
-projects = load_projects()
-profiles = load_profiles()
-
-
-@app.route('/')
-def index():
-    return render_template('index.html', profiles=profiles, projects=projects, title="Team Kenargi's portfolio",
-                           url=base_url)
+# projects = load_projects()
+# profiles = load_profiles()
 
 
-@app.route('/projects/<name>')
-def get_project(name):
-    if name not in projects:
-        return abort(404)
-    return render_template('project.html', item=projects[name], title=name, url=projects_base_url + name)
+# @app.route('/')
+# def index():
+#     return render_template('index.html', profiles=profiles, projects=projects, title="Team Kenargi's portfolio",
+#                            url=base_url)
 
 
-@app.route('/profiles/<name>')
-def get_profile(name):
-    if name not in profiles:
-        return abort(404)
-    title = name + "'s Profile"
-    return render_template('profile.html', item=profiles[name], title=title, url=profiles_base_url + name)
+# @app.route('/projects/<name>')
+# def get_project(name):
+#     if name not in projects:
+#         return abort(404)
+#     return render_template('project.html', item=projects[name], title=name, url=projects_base_url + name)
+
+
+# @app.route('/profiles/<name>')
+# def get_profile(name):
+#     if name not in profiles:
+#         return abort(404)
+#     title = name + "'s Profile"
+#     return render_template('profile.html', item=profiles[name], title=title, url=profiles_base_url + name)
 
 
 @app.errorhandler(404)
