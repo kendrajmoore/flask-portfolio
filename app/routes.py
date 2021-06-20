d6cd7bba8c9c9f4d2c824d4f069fbc53949e0e1a
kendrajmoore <kendra.j.moore3443@gmail.com>
2021-06-20 18:00:31 -0400

    add tailwind and header

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 def index():
-    return "Hello, World"
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', title="Page not found"), 404
