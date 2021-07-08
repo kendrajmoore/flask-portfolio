56ed0a088cd7ab70c6425508ab4be56e97c7ac7b
kendrajmoore <kendra.j.moore3443@gmail.com>
2021-07-08 15:28:57 -0400

    added bash script

--- prod-test.sh
+++ prod-test.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-curl --request GET 'https://kjmoore.duckdns.org'
-curl --request GET 'https://kjmoore.duckdns.org/register'
-curl --request GET 'https://kjmoore.duckdns.org/login'
+number="200"
+url=(https://kjmoore.duckdns.org, https://kjmoore.duckdns.org/register, https://kjmoore.duckdns.org/login)
+
+for i in ${url[@]}; do
+    status="$(curl ${url[@]} -I -o headers -s)"
+ 
+    response="$(cat headers | head -n 1 | cut '-d ' '-f2')"
+    if [ "${response}" -eq ${number} ] ; then
+        echo "0"
+    else
+        echo "1"
+    fi
+done
